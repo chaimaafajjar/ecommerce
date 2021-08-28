560160757bd99f2317358d4ee205d78818dcdcda
chaimaafajjar <ch.fajjar99@gmail.com>
2021-08-28 21:59:19 +0200

    version 3

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -9,16 +9,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.Random;
 
 @SpringBootApplication
 public class EcommerceApplication implements CommandLineRunner {
 
+
 	@Autowired
 	private ProductRepository productRepository;
 	@Autowired
 	private CategoryRepository categoryRepository;
+@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
@@ -26,6 +31,7 @@ public class EcommerceApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+      repositoryRestConfiguration.exposeIdsFor(Product.class,Category.class);
 
 		categoryRepository.save(new Category(null,"Ordinateur",null,null,null));
 		categoryRepository.save(new Category(null,"Phone",null,null,null));
